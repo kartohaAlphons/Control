61c9148ca1fda49fb9de18a243e9ce19cde8b54b
Maksim Komar <mr.bomber1993@gmail.com>
2023-06-12 12:34:42 +0300

    firebase crashlitics added

--- Control/AppMain/AppDelegate.swift
+++ Control/AppMain/AppDelegate.swift
@@ -1,11 +1,6 @@
-//
-//  AppDelegate.swift
-//  Control
-//
-//  Created by Максим Комар on 1.04.23.
-//
-
+ 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
